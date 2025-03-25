7ef9dd6ce937b4add674eb9e396933db1bb19111
theMungai <mungaithesimon@gmail.com>
2025-03-25 08:45:40 +0300

    Populate images from JSON file to the DOM

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function fetchBooks(){
     fetch("http://localhost:3000/books")
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((books) => books.forEach((book) => populateBooks(book)))
 }
 fetchBooks()
+
+const booksGrid = document.querySelector(".books-grid");
+booksGrid.innerHTML = ""
+function populateBooks(book){
+    let bookPoster = document.createElement("li")
+    bookPoster.innerHTML = `<img src = "${book.image_url}">`
+    booksGrid.appendChild(bookPoster)
+    
+}
